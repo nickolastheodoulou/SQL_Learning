6d721683ea557da658e4d79106a999603dd29dea
NickTheo96 <nicktheodoulou96@gmail.com>
2018-11-23 16:39:50 +0000

    - Database stored in memory

--- Resource2/sqlite_demo.py
+++ Resource2/sqlite_demo.py
@@ -1,54 +1,68 @@
 import sqlite3
 from Resource2.employee import Employee
 
-# creates the data base
-conn = sqlite3.connect('employee.db')
+# creates the data base in memory
+conn = sqlite3.connect(':memory:')
 
 c = conn.cursor()
 
 # create a table called employees with 3 columns, first, last, pay
-'''
 c.execute("""CREATE TABLE employees(
     first text,
     last text,
     pay integer
     )""")
-'''
+
+def insert_emp(emp):
+    with conn:
+        c.execute("INSERT INTO  employees VALUES (:first, :last, :pay)",
+                  {'first': emp.first, 'last': emp.last, 'pay': emp.pay})
+        # bad practice to insert python variables using this method
+        #  c.execute("INSERT INTO  employees VALUES ('{}', '{}', '{}')".format(emp_1.first, emp_1.last, emp_1.pay))
+
+        # other correct method that doesnt allow sql injection
+        # c.execute("INSERT INTO  employees VALUES (?, ?, ?)", (emp_1.first, emp_1.last, emp_1.pay))
+
+
+def get_emps_by_name(lastname):
+    c.execute("SELECT * FROM employees WHERE last =:last ", {'last': lastname})
+    # other method to select names
+    # c.execute("SELECT * FROM employees WHERE last =?", ('Schafer',))
+    return  c.fetchall()
+
+
+def update_pay(emp, pay):
+    with conn:
+        c.execute("""UPDATE employees SET pay = :pay
+                    WHERE first = :first AND last = :last""",
+                  {'first': emp.first, 'last': emp.last, 'pay': pay})
+
+
+def remove_emp(emp):
+    with conn:
+        c.execute("DELETE from employees WHERE first = :first AND last = :last",
+                  {'first': emp.first, 'last': emp.last})
+
 
 emp_1 = Employee('John', 'Doe', 80000)
 emp_2 = Employee('Jane', 'Doe', 90000)
 
-print(emp_1.first)
+insert_emp(emp_1)
+insert_emp(emp_2)
 
-# bad practice to insert python variables using this method
-#  c.execute("INSERT INTO  employees VALUES ('{}', '{}', '{}')".format(emp_1.first, emp_1.last, emp_1.pay))
-
-# correct method that doesnt allow sql injection
-# c.execute("INSERT INTO  employees VALUES (?, ?, ?)", (emp_1.first, emp_1.last, emp_1.pay))
-
-# other correct method using a dictionary instead of a tuple
-# c.execute("INSERT INTO  employees VALUES (:first, :last, :pay)",
-#          {'first': emp_2.first, 'last': emp_2.last, 'pay': emp_2.pay})
-
-# inserts a column into the table once it has been created
-# c.execute("INSERT INTO employees VALUES('Corey', 'Schafer',50000) ")
-# c.execute("INSERT INTO employees VALUES('Mary', 'Schafer',70000) ")
-# conn.commit()
-
-# method to select names
-c.execute("SELECT * FROM employees WHERE last =?", ('Schafer',))
-print(c.fetchall())
-
-# other method to select names
-c.execute("SELECT * FROM employees WHERE last =:last ", {'last': 'Doe'})
-# print all values
-print(c.fetchall())
+emps = get_emps_by_name('Doe')
+print(emps)
 
 
-# prints one value
-# print(c.fetchone())
+update_pay(emp_2, 95000)
+remove_emp(emp_1)
 
-conn.commit()
+emps = get_emps_by_name('Doe')
+print(emps)
 
-# close connection to the database
+'''
+emps = get_emps_by_name('Doe')
+print(emps)
+'''
+
 conn.close()
